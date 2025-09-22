b8d5b50acb489fb4a9da7d6b0984998f6aee9007
Kaitest0 <khine.test@gmail.com>
2025-09-22 16:05:01 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -76,6 +76,86 @@ namespace VirtualPetSimulatorProject
                     string choice = Console.ReadLine();
                     Console.WriteLine();
 
+                    /* Pet Care Action Start **/
+
+                    if (UserChoice == "1") // Decreases hunger, slightly increases health
+                    {
+                        PetHunger -= 2;
+                        PetHealth += 2;
+                    
+                        Console.WriteLine("You fed " + petName + ", his hunger decreses and health improves slightly");
+                    }
+                    else if (UserChoice == "2") // Increases happiness, increases hunger
+                    {
+                        if (PetHunger >= 8)
+                        {
+                            Console.WriteLine(petName + " is too hungry to play!");
+                        }
+                        else
+                        {
+                            PetHappiness += 2;
+                            PetHunger += 2;
+                        }
+                    
+                        Console.WriteLine("You played with " + petName + ". His happiness increases, but he is a bit hungrier.");
+                    }
+                    else if (UserChoice == "3") // Improves health, decreases happiness.
+                    {
+                        PetHealth += 2;
+                        PetHappiness -= 2;
+                    
+                        Console.WriteLine(petName + " feels rested.");
+                    }
+                    else if (UserChoice == "4") // Check the pet's status
+                    {
+                        Console.WriteLine(petName + "'s Status");
+                        Console.WriteLine("Pet Hunger: " + PetHunger + "/10");
+                        Console.WriteLine("Pet Happiness: " + PetHappiness + "/10");
+                        Console.WriteLine("Pet Health: " + PetHealth + "/10");
+                        Console.WriteLine();
+                    
+                    }
+                    else if (UserChoice == "5") // User chose to exit the app
+                    {
+                        validStatus = false;
+                        Console.WriteLine("Thank you for playing with " + petName + ". Goodbye!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input. Please try again");
+                    }
+                    
+                    /* Resetting the value to keep them between 0 to 10 */
+                    
+                    if (PetHappiness < 0)
+                    {
+                        PetHappiness = 0;
+                    }
+                    else if (PetHappiness > 10)
+                    {
+                        PetHappiness = 10;
+                    }
+                    
+                    if (PetHealth < 0)
+                    {
+                        PetHealth = 0;
+                    }
+                    else if (PetHealth > 10)
+                    {
+                        PetHealth = 10;
+                    }
+                    
+                    if (PetHunger < 0)
+                    {
+                        PetHunger = 0;
+                    }
+                    else if (PetHunger > 10)
+                    {
+                        PetHunger = 10;
+                    }
+                    
+                    /* Pet Care Action End */
+
 
                 } // end of while loop
 
